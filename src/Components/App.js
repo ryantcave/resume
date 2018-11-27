5218e47ff6915ffc694a5856e7aeb0a64da25c9e
Ryan Cave <Ryan_Cave@baylor.edu>
2018-11-27 14:43:52 -0600

    Avatar layout modification.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,12 @@
 import React, { Component, Fragment } from "react";
 import { Header, Footer} from './Layouts';
+import Avatar from './Layouts/Avatar';
 
 export default class extends Component {
     render() {
         return <Fragment>
             < Header />
+            < Avatar />
             < Footer />
         </Fragment>
     }
