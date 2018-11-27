f312a69867b74056b9569a516d45d6015d54adee
Ryan Cave <Ryan_Cave@baylor.edu>
2018-11-26 22:37:43 -0600

    Revised folder structure, fixed main index, added footer/headers.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import App from "./Components/App";
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
-const title = 'My Minimal React Webpack Babel Setup';
+const theme = createMuiTheme({
+    palette: {
+        type: 'dark',
+        primary: {
+            // light: will be calculated from palette.primary.main,
+            main: '#12150f',
+            // dark: will be calculated from palette.primary.main,
+            // contrastText: will be calculated to contrast with palette.primary.main
+        },
+        secondary: {
+            light: '#0066ff',
+            main: '#0044ff',
+            // dark: will be calculated from palette.secondary.main,
+            contrastText: '#ffcc00',
+        },
+        // error: will use the default color
+    },
+});
 
-ReactDOM.render(
-  <div>{title}</div>,
-  document.getElementById('app')
-);
 
-module.hot.accept();
+render(<MuiThemeProvider theme={theme}><App /></MuiThemeProvider>, document.getElementById("root"));
